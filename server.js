08a7e1d9d792eb7598b12790392ac076196b5f42
joe <joe.dev.public@gmail.com>
2022-05-30 17:05:01 +0100

    Uncomment login route in server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cookieParser = require("cookie-parser");
 const home = require("./routes/home");
 const signUp = require("./routes/signup");
 const posts = require("./routes/posts");
-//const login = require("./routes/login");
+const login = require("./routes/login");
 //const logout = require("./routes/logout");
 
 const bodyHandler = express.urlencoded({ extended: false });
@@ -23,8 +23,8 @@ server.post("/signup", signUp.post);
 server.get("/posts", posts.get);
 server.post("/posts", posts.post);
 
-// server.get("/login", login.get);
-// server.post("/login", login.post);
+server.get("/login", login.get);
+server.post("/login", login.post);
 
 //server.post("/logout", logout.post);
 
