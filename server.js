fd03b651e68f05a82501b6960a4fe653a86d117f
Alex Grafen <alex.grafen@gmail.com>
2022-05-30 15:30:04 +0100

    add further paths

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const server = express();
 
-// const home = require("./routes/home");
+const home = require("./routes/home");
 const signUp = require("./routes/signup");
+const posts = require("./routes/posts");
+const login = require("./routes/login");
+const logout = require("./routes/logout");
 
 const bodyHandler = express.urlencoded({ extended: false });
 const staticHandler = express.static("public");
@@ -10,10 +13,20 @@ const staticHandler = express.static("public");
 server.use(bodyHandler);
 server.use(staticHandler);
 
-// server.get("/", home.get);
+server.get("/", home.get);
+
 
 server.get("/signup", signUp.get);
 server.post("/signup", signUp.post);
+
+server.get("/posts", posts.get);
+server.post("/posts", posts.post);
+
+server.get("/login", login.get);
+server.post("/login", login.post);
+
+server.post("/logout", logout.post);
+
 
 const PORT = process.env.PORT || 3333;
 
