a08818e7965b1bfd43843402cc93406737575050
joe <joe.dev.public@gmail.com>
2022-05-31 10:20:30 +0100

    Progress with first test

--- cypress/integration/tests.js
+++ cypress/integration/tests.js
@@ -7,9 +7,35 @@ it("test test :)", () => {
   cy.visit("/");
 });
 
-it("/login route shows login form", () => {
-  cy.visit("/login");
-  cy.get("form");
+describe("Signing up and logging in", () => {
+  // Add a user, log in as them, make sure the cookie is set
+
+  const username = "auniqueuserfortesting";
+  const password = "aspecialpasswordjustfortesting123";
+
+  const usernameInputSelector = "input[name='username']";
+  const passwordInputSelector = "input[id='password']";
+
+  const sessionIdCookieName = "sid";
+
+  it("Submit signup form on /signup route", () => {
+    cy.visit("/signup");
+    cy.get(usernameInputSelector).type(username);
+    cy.get(passwordInputSelector).type(password);
+    cy.get("form").submit();
+  });
+
+  it("Given session cookie and redirected to /posts route", () => {
+    cy.getCookie(sessionIdCookieName);
+    cy.url().should("eq", Cypress.config().baseUrl + "posts");
+  });
+
+  // it("Submit login form on /login route", () => {
+  //   cy.visit("/login");
+  //   cy.get(usernameInputSelector).type(username);
+  //   cy.get(passwordInputSelector).type(password);
+  //   cy.get("form").submit();
+  // });
 });
 
 after(() => {
