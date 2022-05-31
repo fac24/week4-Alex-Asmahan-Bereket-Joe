da1209f1b2ee37e110e49c2fabc42c215fbd8171
Alex Grafen <alex.grafen@gmail.com>
2022-05-31 11:18:36 +0100

    access user information

--- routes/logout.js
+++ routes/logout.js
@@ -1,9 +1,25 @@
 const auth = require("../auth");
+const layout = require("../layout");
+const model = require("../database/model");
 
 function post(req, res) {
-    const { username, password } = req.body;
-    auth
-        .verifyUser(username, password)
+    const sid = req.signedCookies.sid;
+    return model.getSession(sid).then((result) => {
+
+        return result;
+    })
+        .then((result) => {
+            console.log(8)
+            console.log(result.data.user.username);
+            // console.log(result.data.password);
+            const username = result.data.user.username;
+            const password = result.data.user.password;
+            console.log(16);
+            console.log(username);
+            console.log(password);
+            return auth
+                .verifyUser(username, password)
+        })
         .then((verification) => {
             if (verification === false) {
                 throw new Error();
@@ -11,17 +27,17 @@ function post(req, res) {
                 return auth.endSession(verification);
             }
         })
-        .then((res.send("/")))
         .catch(() => {
-            res
+            return res
                 .status(401)
                 .send(
                     layout(
                         `Error`,
-                        `<h1 class="error-message"> Something went wrong</h1></h1>`
+                        `<h1 class="error-message"> Something went wrong</h1>`
                     )
                 );
-        });
+        })
+        .then((res.redirect("/"))
+        )
 }
-
 module.exports = { post }
